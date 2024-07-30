00100c6ab9fbf94214c26c8160ccdeec69672baa
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-07-30 17:57:19 -0400

    Completado hasta martes

---
+++ resources/javascript/index.js
@@ -0,0 +1,58 @@
+// Variables
+
+const menuIcon = document.querySelector(".menu-icono");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+
+// Menu que se esconde con el botoncito arriba a la derecha
+
+function openMenu() {
+    document.body.classList.toggle("show-scroll");
+    menu.classList.toggle("show-menu");
+    menuIcon.classList.toggle("show-menu-icono", false);
+}
+
+function closeMenu() {
+    document.body.classList.toggle("show-scroll", false);
+    menu.classList.toggle("show-menu", false);
+    menuIcon.classList.toggle("show-menu-icono", true);
+}
+
+menuIcon.addEventListener("click", openMenu);
+menu.addEventListener("click", closeMenu);
+
+const showMenuIcon = new IntersectionObserver(
+    (entries) => {
+        if (entries[0].isIntersecting) {
+            menuIcon.classList.toggle("show-menu-icono", false);
+        } else {
+            menuIcon.classList.toggle("show-menu-icono", true);
+        }
+    },
+    { threshold: 0.3 }
+);
+
+showMenuIcon.observe(nav);
+
+//titulos de las secciones
+
+const contenido = document.querySelectorAll(".contenido");
+const contenidoTitulo = document.querySelectorAll(".contenido-titulo");
+
+const observerContenido = new IntersectionObserver(
+  (entries) => {
+    for (let i = 0 ; i < entries.length ; i++) {
+        if (entries[i].isIntersecting) {
+            console.log(i);
+            contenidoTitulo[i].classList.toggle("show-titulo", true);
+        } else {
+            contenidoTitulo[i].classList.toggle("show-titulo", false);
+        }
+    }
+  }, 
+  { threshold: 0.5 }
+);
+
+for (j = 0 ; j < contenido.length ; j++) {
+    observerContenido.observe(contenido[j]);
+}
