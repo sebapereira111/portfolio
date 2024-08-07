cec6cbd5b8afc2e382989dc9ed6f4f75223b95b0
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-08-07 17:31:58 -0400

    Completed comments

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,28 +1,39 @@
+// Javascript is separated in 4 sections to be more organized
+
+/* The menu is hidden and can only be shown with the press of the menu button (menu-icono) */
+/* Menu button is hidden on landing page and it appears when nav is scrolled out off screen (70% of it) */
+
 // Variables
-
+// The menu button
 const menuIcon = document.querySelector(".menu-icono");
+// The menu itself
 const menu = document.querySelector(".menu");
+// The nav from the top of the page
 const nav = document.querySelector("nav");
-const contenido = document.querySelectorAll(".contenido");
-const contenidoTitulo = document.querySelectorAll(".contenido-titulo");
 
-// Menu que se esconde con el botoncito arriba a la derecha
-
+// To show the menu
 function openMenu() {
-    document.body.classList.toggle("show-scroll");
-    menu.classList.toggle("show-menu");
+    // Scroll is disabled on body
+    document.body.classList.toggle("show-scroll", true);
+    // Menu is shown (moved from above viewport to in front of it)
+    menu.classList.toggle("show-menu", true);
+    // Menu icon is hidden (on the same corner but outside the viewport)
     menuIcon.classList.toggle("show-menu-icono", false);
 }
 
+// To hide the menu (The oposite of openMenu is done)
 function closeMenu() {
     document.body.classList.toggle("show-scroll", false);
     menu.classList.toggle("show-menu", false);
     menuIcon.classList.toggle("show-menu-icono", true);
 }
 
+// When menu icon is clicked the menu is opened
 menuIcon.addEventListener("click", openMenu);
+// When the menu is clicked the menu is closed
 menu.addEventListener("click", closeMenu);
 
+// The menu icon is shown when 70% of nav is outside the screen
 const showMenuIcon = new IntersectionObserver(
     (entries) => {
         if (entries[0].isIntersecting) {
@@ -36,8 +47,12 @@ const showMenuIcon = new IntersectionObserver(
 
 showMenuIcon.observe(nav);
 
-// Animacion de titulos de las secciones y de los contenidos
+/* Content and title animation */
 
+// The content section, an array
+const contenido = document.querySelectorAll(".contenido");
+
+// When the content is on the 2% central part of the viewport its title is shown and content fade in
 const observerContenido = new IntersectionObserver(
   (entries) => {
     for (const entry of entries) {
@@ -56,11 +71,13 @@ for (j = 0 ; j < contenido.length ; j++) {
     observerContenido.observe(contenido[j]);
 }
 
-// Cambio del contraste del sitio
+/* Theme (dark/light) changing */
 
+// Variables for the button (word) to change (toggle) it
 const dark = document.querySelector("#dark");
 const light = document.querySelector("#light");
 
+// When thene is changed, color variables are reasigned and the button swapped
 const toDark = function() {
     dark.classList.toggle("show-contraste", false);
     light.classList.toggle("show-contraste", true);
@@ -82,9 +99,10 @@ const toLight = function() {
 dark.addEventListener("click", toDark);
 light.addEventListener("click", toLight);
 
-// To detect client system theme
+/* To detect the system theme */
 
 const changeTheme = function() {
+    // color scheme preference is checked and page theme adjusted accordingly
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         toDark();
     } else {
@@ -92,6 +110,8 @@ const changeTheme = function() {
     }
 }
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",changeTheme);
+// A listener is added for when the color scheme changes
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", changeTheme);
 
+// Default theme is dark so when loading page prefered color scheme is checked, if it is light theme is changed
 if (window.matchMedia("(prefers-color-scheme: light)").matches) toLight();
